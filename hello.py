4d1f5f80b1211b26e59a0671c309fd8c64bc1f81
daryle <daryle.tan@gmail.com>
2024-03-01 23:35:43 -0600

    loops

--- hello.py
+++ hello.py
@@ -7,3 +7,18 @@ while n > 0:
     print(n)
     n = n - 1
 print("Blastoff!")
+
+# definite loops
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print('Happy Birthday:', friend)
+print('Done!')
+
+# find the largest number
+largest = -1
+print('Before:', largest)
+for the_num in [9, 41, 12, 3, 74, 15]:
+    if the_num > largest:
+        largest = the_num
+    print(largest, the_num)
+print('After:', largest)
