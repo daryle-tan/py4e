7ed4cef0e2b8baa7a149356dc3b2a0d23e85c49f
daryle <daryle.tan@gmail.com>
2024-03-23 19:24:56 -0500

    fin ex_07_02

---
+++ ex_07_01.py
@@ -0,0 +1,30 @@
+# fh = open('mbox-short.txt')
+
+# for lx in fh:
+#     ly = lx.rstrip()
+#     print(ly.upper())
+
+
+# 7.2
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+count = 0
+tot = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+# extract floating point values from each line
+    count = count + 1
+    pos = line.find(':')
+    line = line[pos+1:]
+    line = line.strip()
+    fval = float(line)
+    tot = tot + fval
+    avg = tot / count
+# compute the average of those values
+print('Average spam confidence:', avg)
+print("Done")
