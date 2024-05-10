53b9d8fcef40ce85d4903a44c6f94cd1d30f878c
daryle <daryle.tan@gmail.com>
2024-05-09 22:15:29 -0700

    iterate through a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -13,3 +13,12 @@ print(len(eng2sp)) # 3
 
 vals = list(eng2sp.values())
 print(list(eng2sp.values()))
+
+word = 'brontosaurus'
+d = dict()
+for c in word:
+    if c not in d:
+        d[c] = 1
+    else:
+        d[c] = d[c] + 1
+print(d) # {'b': 1, 'r': 2, 'o': 2, 'n': 1, 't': 1, 's': 2, 'a': 1, 'u': 2}
