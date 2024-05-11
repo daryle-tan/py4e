4d23284c20e8c385d23848fb93d44ab3910cea8a
daryle <daryle.tan@gmail.com>
2024-05-10 19:59:38 -0700

    get method

--- dictionaries.py
+++ dictionaries.py
@@ -22,3 +22,13 @@ for c in word:
     else:
         d[c] = d[c] + 1
 print(d) # {'b': 1, 'r': 2, 'o': 2, 'n': 1, 't': 1, 's': 2, 'a': 1, 'u': 2}
+
+# get method
+counts = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
+print(counts.get('jan', 0)) # 100
+
+word = 'brontosaurus'
+d = dict()
+for c in word:
+    d[c] = d.get(c, 0) + 1
+print(d) # {'b': 1, 'r': 2, 'o': 2, 'n': 1, 't': 1, 's': 2, 'a': 1, 'u': 2}
