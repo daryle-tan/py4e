7a19039ed606faa9bfbc3af19075f43130faf768
daryle <daryle.tan@gmail.com>
2024-05-13 23:08:22 -0500

    use for in loop to determine if a key is found in a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -32,3 +32,13 @@ d = dict()
 for c in word:
     d[c] = d.get(c, 0) + 1
 print(d) # {'b': 1, 'r': 2, 'o': 2, 'n': 1, 't': 1, 's': 2, 'a': 1, 'u': 2}
+
+# you can't look up if a non exisitent key is in a dictinary otherwise you will get an error
+# print(counts['csev']) # KeyError: 'csev'
+# therefore in order to accomplis this we must use the for in loop
+for key in counts:
+    if key not in counts:
+        counts[key] = 1
+    else:
+        counts[key] = counts[key] + 1
+print(counts) # {'chuck': 2, 'annie': 2, 'jan': 2}
